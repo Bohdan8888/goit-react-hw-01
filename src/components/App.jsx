f593903f518b666d51a6a1e019eb213e203e37e5
Bohdan <bogdaansharyga@gmail.com>
2024-04-29 12:32:34 +0300

    redact

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import Profile from './Profile/Profile';
+import userData from "./Profile/userData.json";
+import FriendList from "./Friend/FriendList";
+import friends from "./Friend/friends.json";
+import transactions from "./Transaction/transactions.json";
+import TransactionHistory from "./Transaction/TransactionHistory";
+
+import css from "./App.module.css";
+
+
+
+export default function App() {
+  return (
+    <div className={css.container}>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
+    </div>
+  );
+}
+   
+  
+
